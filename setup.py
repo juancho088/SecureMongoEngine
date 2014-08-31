1d21e30cc83d40ff468d6a4b24072fa35fcecab2
juancho088 <juancho088@gmail.com>
2014-08-31 16:10:33 -0500

    New software version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = "SecureMongoEngine",
-    version = "0.1.1",
+    version = "0.1.2",
     packages = find_packages(),
     install_requires = ['pymongo>=2.7.2','mongoengine>=0.8.7','pycrypto>=2.6.1'],
 
